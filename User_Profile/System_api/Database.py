524b36664b6e191c2c2c9197e35512fd9bb7dc34
Yeshwanth B E <yeshwanthbe19@gmail.com>
2023-09-22 11:38:44 +0530

    added admin previlages

--- User_Profile/System_api/Database.py
+++ User_Profile/System_api/Database.py
@@ -4,28 +4,32 @@ import yaml
 import json
 import os
 class User:
-    def __init__(self,filepath):
+    def __init__(self,filepath,ap=0):
+        table= "admin" if ap else "users"
         with open(filepath, 'r') as file:
             db_config= yaml.safe_load(file)['database']    
             self.db = mc.connect(host=db_config['host'],user=db_config['username'],password=db_config['password'],database=db_config['dbname'])
         self.cursor=self.db.cursor()
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS Users (mailid VARCHAR(255) PRIMARY KEY,password VARCHAR(256) NOT NULL,salt varbinary(255),name VARCHAR(255),address VARCHAR(255),phno VARCHAR(255));")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS %s (mailid VARCHAR(255) PRIMARY KEY,password VARCHAR(256) NOT NULL,salt varbinary(255),name VARCHAR(255),address VARCHAR(255),phno VARCHAR(255));",table)
         self.db.commit()
         
-    def isexists(self,mailid):
-        self.cursor.execute("select mailid from users where mailid=%s",(mailid,))
+    def isexists(self,mailid,ap=0):
+        table= "admin" if ap else "users"
+        self.cursor.execute("select mailid from %s where mailid=%s",(self.table,mailid))
         if self.cursor.fetchone() is None:
             return False
         return True
     
-    def add_user(self,user_json:json):
+    def add_user(self,user_json:json,ap=0):
+        table= "admin" if ap else "users"
         salt=os.urandom(16)
         pwd=hashlib.sha256(user_json['password'].encode()+salt).hexdigest()
-        self.cursor.execute("Insert into Users values (%s,%s,%s,%s,%s,%s)",(user_json['mailid'],pwd,salt,user_json['name'],user_json['address'],user_json['phno']))
+        self.cursor.execute("Insert into %s values (%s,%s,%s,%s,%s,%s)",(self.table,user_json['mailid'],pwd,salt,user_json['name'],user_json['address'],user_json['phno']))
         self.db.commit()
 
-    def show_user(self,mailid:str) -> json:
-        self.cursor.execute("Select * from Users where mailid=%s",(mailid,))
+    def show_user(self,mailid:str,ap=0) -> json:
+        table= "admin" if ap else "users"
+        self.cursor.execute("Select * from %s where mailid=%s",(self.table,mailid))
         user=self.cursor.fetchone() 
         result_dict = {
             "mailid": user[0],
@@ -35,28 +39,31 @@ class User:
             }
         return json.dumps(result_dict, indent=4)
     
-    def del_user(self,mailid:str):
-        self.cursor.execute("delete from Users where mailid=%s;",(mailid,))
+    def del_user(self,mailid:str,ap=0):
+        table= "admin" if ap else "users"
+        self.cursor.execute("delete from %s where mailid=%s;",(self.table,mailid))
         self.db.commit()
 
-    def update_user(self,mailid,user_json):
-        query="update users set {}=%s,{}=%s,{}=%s where mailid=%s;".format('name','address','phno')
-        self.cursor.execute(query,(user_json['name'],user_json['address'],user_json['phno'],mailid))
+    def update_user(self,mailid,user_json,ap=0):
+        table= "admin" if ap else "users"
+        query="update %s set {}=%s,{}=%s,{}=%s where mailid=%s;".format('name','address','phno')
+        self.cursor.execute(query,(self.table,user_json['name'],user_json['address'],user_json['phno'],mailid))
         self.db.commit()
 
-    def reset_pass(self,mailid,password):
-        self.cursor.execute("select salt from users where mailid=%s",(mailid,))
-        salt=self.cursor.fetchone()[0]
-        pwd=hashlib.sha256(password.encode()+salt).hexdigest()
-        self.cursor.execute("update users set password=%s where mailid=%s;",(pwd,mailid))
-        self.db.commit()
-
-    def Auth(self,mailid,password,ap):
+    def reset_pass(self,mailid,password,ap=0):
         table= "admin" if ap else "users"
         self.cursor.execute("select salt from %s where mailid=%s",(self.table,mailid))
         salt=self.cursor.fetchone()[0]
         pwd=hashlib.sha256(password.encode()+salt).hexdigest()
-        self.cursor.execute("select password from users where mailid=%s;",(mailid,))
+        self.cursor.execute("update %s set password=%s where mailid=%s;",(self.table,pwd,mailid))
+        self.db.commit()
+
+    def Auth(self,mailid,password,ap=0):
+        table= "admin" if ap else "users"
+        self.cursor.execute("select salt from %s where mailid=%s",(self.table,mailid))
+        salt=self.cursor.fetchone()[0]
+        pwd=hashlib.sha256(password.encode()+salt).hexdigest()
+        self.cursor.execute("select password from %s where mailid=%s;",(self.table,mailid))
         if pwd in self.cursor.fetchone():
             return True
         else:
--- User_Profile/System_api/dbroutes.py
+++ User_Profile/System_api/dbroutes.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request,make_response,session,flash,jsonify
+from flask import Flask, request,make_response,jsonify
 from Database import User
 
 app=Flask(__name__) 
@@ -7,41 +7,40 @@ obj=User(os.getcwd()+"\\User_Profile\\System_api\\config.yaml")
 
 @app.route('/profile/Signup/<int:ap>',methods=['POST'])
 def signup(ap):
-    response_data = {"message": "User Registered Successfully"}
-    return jsonify(response_data), 201
+    obj.add_user(request.get_json(),ap)
     response_data = {"message": "User Registered Successfully"}
     return jsonify(response_data), 201
 
-@app.route('/profile/<mailid>',methods=['GET','POST','DELETE','PATCH'])
-def profile(mailid):
+@app.route('/profile/<mailid>/<int:ap>',methods=['GET','POST','DELETE','PATCH'])
+def profile(mailid,ap):
     if request.method=='GET':
-        return jsonify(obj.show_user(mailid))
+        return jsonify(obj.show_user(mailid,ap))
     elif request.method=='DELETE':
-        obj.del_user(mailid)
+        obj.del_user(mailid,ap)
         response_data = {"message": "User Deleted Successfully"}
         return jsonify(response_data), 204
     elif request.method=='POST':
         data=request.get_json()
-        obj.reset_pass(mailid,data['newpwd'])
+        obj.reset_pass(mailid,data['newpwd'],ap)
         response_data = {"message": "Password Resetted Successfully"}
         return jsonify(response_data), 200
     else:
         data=request.get_json()
-        obj.update_user(mailid,data)
+        obj.update_user(mailid,data,ap)
         response_data = {"message": "Profile Updated Successfully"}
         return jsonify(response_data), 200  
     
-@app.route('/profile/<mailid>/exists')
-def exists(mailid):
-    if obj.isexists(mailid) is True:
+@app.route('/profile/<mailid>/<int:ap>/exists')
+def exists(mailid,ap):
+    if obj.isexists(mailid,ap) is True:
         return jsonify({'message':'mailid exists'}),200
     else:
         return jsonify({"message":'mailid not exists'}),404
     
-@app.route('/profile/<mailid>/auth',methods=['POST'])
-def auth(mailid):
+@app.route('/profile/<mailid>/<int:ap>/auth',methods=['POST'])
+def auth(mailid,ap):
     pwd=request.get_json().get('password')
-    if obj.Auth(mailid,pwd):
+    if obj.Auth(mailid,pwd,ap):
         return jsonify({"message":"Authentication Successful"}),200
     return jsonify({"message":"Invalid Mailid or password"}),401
 
