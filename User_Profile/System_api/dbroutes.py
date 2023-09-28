20f411a5216aadc6fe9eee7ab9618c9f56fc8d8e
Yeshwanth B E <yeshwanthbe19@gmail.com>
2023-09-28 20:39:31 +0530

    feat: Exception Handling

--- User_Profile/System_api/dbroutes.py
+++ User_Profile/System_api/dbroutes.py
@@ -25,62 +25,78 @@ def token_required(f):
 
 @app.route('/dbprofile/<int:ap>/Signup/',methods=['POST'])
 def signup(ap):
-    obj=User(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
-    if obj.add_user(request.get_json(),ap) is not False:
-        return jsonify({"message": "User Registered Successfully"}), 201
-    else:
-        return jsonify({"message": "Mailid already exist"}), 401
+    try:
+        obj=User()
+        obj.connect(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
+        if obj.add_user(request.get_json(),ap) is not False:
+            return jsonify({"message": "User Registered Successfully"}), 201
+        else:
+            return jsonify({"message": "Mailid already exist"}), 401
+    except Exception as e:
+        return jsonify({"Exception": e}),500
 
 @app.route('/dbprofile/<int:ap>/',methods=['GET','POST','DELETE','PATCH'])
 @token_required
 def profile(mailid,ap):
-    obj=User(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
-    if request.method=='GET':
-        return jsonify(obj.show_user(mailid,ap))
-    elif request.method=='DELETE':
-        pwd=request.get_json()["password"]
-        if not obj.Auth(mailid,pwd,ap):
-            return jsonify({"message": "Invalid Password"}),401
-        obj.del_user(mailid,ap)
-        return jsonify({"message": "User Deleted Successfully"}), 200
-    elif request.method=='POST':
-        data=request.get_json()
-        obj.reset_pass(mailid,data['password'],ap)
-        return jsonify({"message": "Password Resetted Successfully"}), 200
-    else:
-        data=request.get_json()
-        obj.update_user(mailid,data,ap)
-        return jsonify({"message": "Profile Updated Successfully"}), 200  
+    try:
+        obj=User()
+        obj.connect(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
+        if request.method=='GET':
+            return jsonify(obj.show_user(mailid,ap))
+        elif request.method=='DELETE':
+            pwd=request.get_json()["password"]
+            if not obj.Auth(mailid,pwd,ap):
+                return jsonify({"message": "Invalid Password"}),401
+            obj.del_user(mailid,ap)
+            return jsonify({"message": "User Deleted Successfully"}), 200
+        elif request.method=='POST':
+            data=request.get_json()
+            obj.reset_pass(mailid,data['password'],ap)
+            return jsonify({"message": "Password Resetted Successfully"}), 200
+        else:
+            data=request.get_json()
+            obj.update_user(mailid,data,ap)
+            return jsonify({"message": "Profile Updated Successfully"}), 200  
+    except Exception as e:
+        return jsonify({"Exception": e}),500
     
 @app.route('/dbprofile/<int:ap>/auth/',methods=['POST'])
 def auth(ap):
-    data=request.get_json()
-    obj=User(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
-    if obj.Auth(data['mailid'],data['password'],ap):
-        payload={"mailid": data['mailid'],
-         "exp": int(time.time())+86400
-        }
-        algorithm="HS256" if ap else "HS512"
-        token=jwt.encode(payload,app.config['SECRET_KEY'],algorithm=algorithm)
-        return jsonify({"jwt":token}),200
-    return jsonify({"message":"Invalid Mailid or password"}),401
+    try:
+        data=request.get_json()
+        obj=User()
+        obj.connect(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
+        if obj.Auth(data['mailid'],data['password'],ap):
+            payload={"mailid": data['mailid'],
+            "exp": int(time.time())+86400
+            }
+            algorithm="HS256" if ap else "HS512"
+            token=jwt.encode(payload,app.config['SECRET_KEY'],algorithm=algorithm)
+            return jsonify({"jwt":token}),200
+        return jsonify({"message":"Invalid Mailid or password"}),401
+    except Exception as e:
+        return jsonify({"Exception": e}),500
 
 @app.route('/dbprofile/<int:ap>/tk/',methods=['GET','POST'])
 def tokenauth(ap):
-    obj=User(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
-    if request.method=='GET':
-        mailid=request.args.get('mailid')
-        payload={"mailid": mailid,
-         "exp": int(time.time())+300
-        }
-        algorithm="HS256" if ap else "HS512"
-        token=jwt.encode(payload,app.config['SECRET_KEY'],algorithm=algorithm)
-        return jsonify({"token": obj.token(mailid),
-                        "jwt": token})
-    else:
-        data=request.get_json()
-        obj.token(data['mailid'],data['token'])
-        return jsonify({}),200
+    try:
+        obj=User()
+        obj.connect(os.getcwd()+"\\User_Profile\\System_api\\config.yaml",ap)
+        if request.method=='GET':
+            mailid=request.args.get('mailid')
+            payload={"mailid": mailid,
+            "exp": int(time.time())+300
+            }
+            algorithm="HS256" if ap else "HS512"
+            token=jwt.encode(payload,app.config['SECRET_KEY'],algorithm=algorithm)
+            return jsonify({"token": obj.token(mailid),
+                            "jwt": token})
+        else:
+            data=request.get_json()
+            obj.token(data['mailid'],data['token'])
+            return jsonify({}),200
+    except Exception as e:
+        return jsonify({"Exception": e}),500
 
 if __name__ == '__main__':
    app.run(debug = True)  
