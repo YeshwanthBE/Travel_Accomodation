d0271292ee9a698b2ac105e9de9e50427f61ac50
Yeshwanth B E <yeshwanthbe19@gmail.com>
2023-09-30 16:38:29 +0530

    added rating/reviews inital

---
+++ Reviews_Ratings/System_api/dbratrev.py
@@ -0,0 +1,71 @@
+from datetime import datetime as dt,timedelta as td
+import mysql.connector as mc
+import hashlib
+import yaml 
+import json
+import os
+class rr:
+    def connect(self,filepath):
+        try:
+            with open(filepath, 'r') as file:
+                db_config= yaml.safe_load(file)['database']    
+                self.db = mc.connect(host=db_config['host'],user=db_config['username'],password=db_config['password'],database=db_config['dbname'])
+            self.db.start_transaction()
+            self.cursor=self.db.cursor()
+            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS reviews (CREATE TABLE IF NOT EXISTS reviews (review_id INT AUTO_INCREMENT PRIMARY KEY, userid VARCHAR(255), acmid VARCHAR(255), rating DECIMAL(3, 2), review TEXT, datetime DATETIME, FOREIGN KEY (userid) REFERENCES users(mailid), FOREIGN KEY (acmid) REFERENCES accommodations(mailid));")
+            self.db.commit()
+        except Exception as e:
+            if self.db:
+                self.db.rollback()
+            raise e
+    
+    def add(self,mailid,rr_json:json):
+        try:
+            self.db.start_transaction()
+            current_datetime = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.cursor.execute(f"Insert into reviews (acmid, userid, rating, review, datetime) values(%s,%s,%s,%s,%s)",(rr_json['acmid'],mailid,rr_json['rating'],rr_json['review'],current_datetime))
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            raise e
+
+    def mod(self,rr_json:json) :
+        try:
+            self.db.start_transaction()
+            self.cursor.execute(f"update reviews set rating=%s,reviews=%s where review_id=%s",(rr_json['rating'],rr_json['review'],rr_json['rid']))
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            raise e
+    
+    def delete(self,rid):
+        try:
+            self.db.start_transaction()
+            self.cursor.execute(f"Select * from reviews where review_id=%s for update",(rid,))
+            self.cursor.fetchone() 
+            self.cursor.execute(f"delete from reviews where review_id=%s",(rid,))
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            raise e
+
+    def showall(self,rr_json):
+        try:
+            query=f'select * from reviews where acmid=%s order by {rr_json["sort"]}'
+            if rr_json.get('desc'):
+                query+=" desc;"
+            self.cursor.execute(query,(rr_json['acmid'],))
+            lst=[]
+            for i in self.cursor.fetchall():
+                result_dict = {
+                "bookingid": i[0],
+                "userid": i[1],
+                "acmid": i[2],
+                "rating": float(i[3]),
+                "review": i[4],
+                "datetime": str(i[5])
+                }
+                lst.append(result_dict)
+            return(json.dumps(lst))
+        except Exception as e:
+            raise e
---
+++ Reviews_Ratings/System_api/ratroutes.py
@@ -0,0 +1,57 @@
+import os
+from flask import Flask, request,make_response,jsonify
+from dbratrev import rr
+from functools import wraps
+import jwt
+app=Flask(__name__) 
+app.config['SECRET_KEY']='a'
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms="HS512")
+            mailid = data['mailid']
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}), 401
+        except jwt.DecodeError:
+            return jsonify({'message': 'Token is invalid'}), 401
+        except:
+            return  jsonify({'message': 'Unauthorized'}), 401
+        return f(mailid,*args, **kwargs)
+    return decorated
+
+@app.route('/dbbk/',methods=['POST','DELETE','PATCH'])
+@token_required
+def booking(mailid):
+    try:
+        obj=rr()
+        qp=request.args
+        obj.connect(os.getcwd()+"\\Reviews_Ratings\\System_api\\config.yaml")
+        if request.method=='PATCH':
+            return jsonify(obj.mod(request.json()))
+        elif request.method=='DELETE':
+            obj.delete(qp.get('rid'))
+            return jsonify({"message": "review Cancelled Successfully"}), 200
+        else:
+            if obj.add(mailid,request.get_json()) is not False:
+                return jsonify({"message": "review added Successfully"}), 201
+            else:
+                return jsonify({"message": "Failed"}), 401 
+    except Exception as e:
+        return jsonify({"Exception": str(e)}),500
+    
+@app.route('/dbbk/allbk/')
+@token_required
+def show(mailid):
+    try:
+        obj=rr()
+        obj.connect(os.getcwd()+"\\Reviews_Ratings\\System_api\\config.yaml")
+        return jsonify(obj.showallbk(request.json())),200
+    except Exception as e:
+        return jsonify({"Exception": str(e)}),500
+    
+if __name__ == '__main__':
+   app.run(debug = True,port=8090)  
