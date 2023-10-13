6414979741d0c10e1a6309f639b69a25eeb434ee
Yeshwanth B E <yeshwanthbe19@gmail.com>
2023-10-13 22:21:52 +0530

    added mailid validation

--- SrcCode/User_Profile/Experience_api/static/js/signup.js
+++ SrcCode/User_Profile/Experience_api/static/js/signup.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var usernameInput = document.getElementById('mailid');
     const passwordInput = document.getElementById("password");
     const passwordErrors = document.getElementById("password-errors");
     const cnfpwdip= document.getElementById("confirm_password");
     const cnfpwdErrors = document.getElementById("cnfpwd-errors");
     const nameInput = document.getElementById("name");
-
+    const submit = document.getElementById("submit");
+    var ue =document.getElementById('username-error')
+    submitbutton=true;
     nameInput.addEventListener("input", function () {
         let name = nameInput.value;
         name = name.replace(/[^A-Za-z]/g, '');
@@ -18,11 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
         togglevisiblity(passwordErrors,false)
         if (password !== confirmPassword) {
             cnfpwdErrors.innerHTML = "<i class='fa fa-exclamation-triangle'></i>Passwords do not match!!";
-            togglevisiblity(cnfpwdErrors,true)
-            
+            togglevisiblity(cnfpwdErrors,true);
+            submitbutton=false;        
         } else {
             cnfpwdErrors.innerHTML = "";
-            togglevisiblity(cnfpwdErrors,false)
+            togglevisiblity(cnfpwdErrors,false);
         }
         setTimeout(function () {
             togglevisiblity(cnfpwdErrors,false);
@@ -51,20 +54,41 @@ document.addEventListener("DOMContentLoaded", function () {
             errors.push("<i class='fa fa-exclamation-triangle'></i>Password should not contain your username.</pre>");
         }
         if(errors.length===1)
-            {togglevisiblity(passwordErrors,false)}
+            {togglevisiblity(passwordErrors,false);}
         else
-            {togglevisiblity(passwordErrors,true)}
+            {togglevisiblity(passwordErrors,true);submitbutton=false;}
         passwordErrors.innerHTML = errors.join("");
         setTimeout(function () {
             togglevisiblity(passwordErrors,false);
         }, 5000);
     });
     function togglevisiblity(element,flag){
+        console.log("toggles")
         if(flag){element.style.opacity=1;
         element.style.zIndex=1;}
         else{element.style.opacity=0;
             element.style.zIndex=-1;
-        submitbutton=flag;
         }
     }
+    submit.addEventListener("click",function(event){
+        passwordInput.dispatchEvent(new Event("input"));
+        cnfpwdip.dispatchEvent(new Event("input"));
+        flag=true;
+        if (usernameInput.value.trim() === '') {
+            togglevisiblity(ue,true);
+            ue.innerHTML = '<i class="fa fa-exclamation-triangle"></i> Mailid is required';
+            flag=false;
+          }
+        if (!submitbutton) {
+            togglevisiblity(passwordErrors,true);
+            flag=false;
+        }
+        if(!flag){
+            event.preventDefault();
+        }
+    usernameInput.addEventListener("click",function(){
+        togglevisiblity(ue,false);
+    });
+    });
+    
 });
