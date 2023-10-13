2de22d40c832879162501546cd233101a4279f65
Yeshwanth B E <yeshwanthbe19@gmail.com>
2023-10-13 20:36:59 +0530

    name validation added

--- SrcCode/User_Profile/Experience_api/static/js/signup.js
+++ SrcCode/User_Profile/Experience_api/static/js/signup.js
@@ -3,12 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const passwordErrors = document.getElementById("password-errors");
     const cnfpwdip= document.getElementById("confirm_password");
     const cnfpwdErrors = document.getElementById("cnfpwd-errors");
+    const nameInput = document.getElementById("name");
 
-    // Event listener for the confirm password field to check for a match
+    nameInput.addEventListener("input", function () {
+        let name = nameInput.value;
+        name = name.replace(/[^A-Za-z]/g, '');
+
+        nameInput.value = name;
+    });
+
     cnfpwdip.addEventListener("input", function () {
         const password = passwordInput.value;
         const confirmPassword = cnfpwdip.value;
-
+        passwordErrors.style.opacity=0;passwordErrors.style.zIndex=-1;
         if (password !== confirmPassword) {
             cnfpwdErrors.innerHTML = "<i class='fa fa-exclamation-triangle'></i>Passwords do not match!!";
             cnfpwdErrors.style.opacity=1;cnfpwdErrors.style.zIndex=1;
@@ -21,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
     passwordInput.addEventListener("input", function () {
         const password = passwordInput.value;
         let errors = ["<i class='fa fa-exclamation-triangle'></i>Must Contain at least &nbsp;"];
-
+        cnfpwdErrors.style.opacity=0;cnfpwdErrors.style.zIndex=-1;
         if (password.length < 8) {
             errors.push("<pre>     &bull; 8 characters.</pre>");
         }
@@ -35,10 +42,10 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!/[!@#$%^&*]/.test(password)) {
             errors.push("<pre>     &bull; one symbol.</pre>");
         }
-        const userName = "example"; // Replace with the actual username.
+        const userName =nameInput.value;
 
         if (password.includes(userName)) {
-            errors.push("Password should not contain your username.");
+            errors.push("<i class='fa fa-exclamation-triangle'></i>Password should not contain your username.</pre>");
         }
         if(errors.length===1)
             {passwordErrors.style.opacity=0;passwordErrors.style.zIndex=-1;}
