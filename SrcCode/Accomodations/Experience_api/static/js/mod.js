ff699828019c782547a4b97dfacf9088fad4b065
Yeshwanth B E <yeshwanthbe19@gmail.com>
2023-10-27 22:20:56 +0530

    added image modification front end

--- SrcCode/Accomodations/Experience_api/static/js/mod.js
+++ SrcCode/Accomodations/Experience_api/static/js/mod.js
@@ -6,21 +6,53 @@ document.addEventListener("DOMContentLoaded", function () {
     const price = document.getElementById("price");
     const description = document.getElementById("description");
     const saveButton = document.getElementById("savebutton");
+    const img=document.getElementById("acmimg");
+    const imgform=document.getElementById("image-upload-form");
+    const imageFileInput = document.getElementById("image-file");
+    const delcon = document.getElementById("delcon");
+    const labelElement = document.querySelector('label.button[for="image-file"]');
+    function initialize(){
+        nsc=nameSpan.textContent
+        asc=addressSpan.textContent
+        psc=phnoSpan.textContent
+        dsc=description.textContent
+        prsc=price.textContent
+        img.style.display="block";
+        imgform.style.display="none";
+        delcon.style.display="none";
+    }
+    function toggledisplay(element){
+        element.style.display=element.style.display==="block"?"none":"block";
+    }
+    imageFileInput.addEventListener("change", function () {
+        if (imageFileInput.files.length > 0) {
+            labelElement.innerHTML=imageFileInput.files[0].name;
+            toggledisplay(delcon);
+        }
+    });
+    delcon.addEventListener("click",function(){
+        imageFileInput.value="";
+        labelElement.innerHTML="Upload";
+        toggledisplay(delcon);
+    });
     function toggleContentAndVisibility() {
-        toggleContentEditable(nameSpan);
-        toggleContentEditable(addressSpan);
-        toggleContentEditable(phnoSpan);
-        toggleContentEditable(description);
-        toggleContentEditable(price);
+        toggleContentEditable(nameSpan,nsc);
+        toggleContentEditable(addressSpan,asc);
+        toggleContentEditable(phnoSpan,psc);
+        toggleContentEditable(description,dsc);
+        toggleContentEditable(price,prsc);
         toggleVisibility(saveButton);
+        toggledisplay(img);
+        toggledisplay(imgform);
+        modifyButton.innerHTML=modifyButton.innerHTML==="Modify"?"Back":"Modify";
     }
 
     modifyButton.addEventListener("click", toggleContentAndVisibility);
 
-    function toggleContentEditable(element) {
+    function toggleContentEditable(element,originalvalue) {
         element.contentEditable = element.contentEditable === "true" ? "false" : "true";
         element.classList.toggle("modify");
-        
+        element.textContent = originalvalue;
     }
     function toggleVisibility(element) {
         element.style.visibility = element.style.visibility === "visible" ? "hidden" : "visible";
@@ -48,9 +80,11 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
         toggleContentAndVisibility();
+        initialize();
     });
     function getcontent(element){
         element.contentEditable="false";
         return element.textContent;
     }
+    initialize();
 });
