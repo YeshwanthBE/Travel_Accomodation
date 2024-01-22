6ee162c48a879f3082a92a7262f66e926c3118bf
Yeshwanth B E <yeshwanthbe19@gmail.com>
2024-01-22 09:23:38 +0530

    bug fix

--- src/User_Profile/Experience_api/static/js/dashboard.js
+++ src/User_Profile/Experience_api/static/js/dashboard.js
@@ -1,49 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const modifyButton = document.getElementById("modifyButton");
-    const nameSpan = document.getElementById("name");
-    const addressSpan = document.getElementById("address");
-    const phnoSpan = document.getElementById("phno");
-    const saveButton = document.getElementById("savebutton");
-    const pfButtons = document.querySelector(".pfbuttons");
-    function toggleContentAndVisibility() {
-        toggleContentEditable(nameSpan);
-        toggleContentEditable(addressSpan);
-        toggleContentEditable(phnoSpan);
-        toggleVisibility(saveButton);
-    }
+  const modifyButton = document.getElementById("modifyButton");
+  const nameSpan = document.getElementById("name");
+  const addressSpan = document.getElementById("address");
+  const phnoSpan = document.getElementById("phno");
+  const saveButton = document.getElementById("savebutton");
+  const pfButtons = document.querySelector(".pfbuttons");
+  function toggleContentAndVisibility() {
+    toggleContentEditable(nameSpan);
+    toggleContentEditable(addressSpan);
+    toggleContentEditable(phnoSpan);
+    toggleVisibility(saveButton);
+    toggleModifyButtonContent();
+  }
+  function toggleModifyButtonContent() {
+    modifyButton.innerText =
+      modifyButton.innerText === "Modify" ? "Back" : "Modify";
+  }
+  modifyButton.addEventListener("click", toggleContentAndVisibility);
 
-    modifyButton.addEventListener("click", toggleContentAndVisibility);
-
-    function toggleContentEditable(element) {
-        element.contentEditable = element.contentEditable === "true" ? "false" : "true";
-        element.classList.toggle("modify");
-    }
-    function toggleVisibility(element) {
-        element.style.visibility = element.style.visibility === "visible" ? "hidden" : "visible";
-        pfButtons.style.height = pfButtons.style.height === "112px"? "56px" :" 112px";
-    }
-    saveButton.addEventListener("click", function() {
-        modifiedname=getcontent(nameSpan)
-        modiifiedaddress=getcontent(addressSpan)
-        modifiedphno=getcontent(phnoSpan)
-        const data = {
-            name: modifiedname,
-            address: modiifiedaddress,
-            phno: modifiedphno
-        };
-        var modUrl = document.getElementById('savebutton').getAttribute('data-mod-url');
-        $.ajax({
-            url: modUrl, // Replace with the correct URL
-            method: 'POST',
-            data: JSON.stringify(data),
-            contentType: 'application/json',
-            success: function(response) {
-            }
-        });
-        toggleContentAndVisibility();
+  function toggleContentEditable(element) {
+    element.contentEditable =
+      element.contentEditable === "true" ? "false" : "true";
+    element.classList.toggle("modify");
+  }
+  function toggleVisibility(element) {
+    element.style.visibility =
+      element.style.visibility === "visible" ? "hidden" : "visible";
+    pfButtons.style.height =
+      pfButtons.style.height === "112px" ? "56px" : " 112px";
+  }
+  saveButton.addEventListener("click", function () {
+    modifiedname = getcontent(nameSpan);
+    modiifiedaddress = getcontent(addressSpan);
+    modifiedphno = getcontent(phnoSpan);
+    const data = {
+      name: modifiedname,
+      address: modiifiedaddress,
+      phno: modifiedphno,
+    };
+    var modUrl = document
+      .getElementById("savebutton")
+      .getAttribute("data-mod-url");
+    $.ajax({
+      url: modUrl, // Replace with the correct URL
+      method: "POST",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+      success: function (response) {},
     });
-    function getcontent(element){
-        element.contentEditable="false";
-        return element.textContent;
-    }
+    toggleContentAndVisibility();
+  });
+  function getcontent(element) {
+    element.contentEditable = "false";
+    return element.textContent;
+  }
 });
