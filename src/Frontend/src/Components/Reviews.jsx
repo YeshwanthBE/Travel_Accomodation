5c731b4ab11ca830187338d88b1e4b5a60027347
Yeshwanth B E <yeshwanthbe19@gmail.com>
2024-02-06 11:54:00 +0530

    added submitreview functionality without jwt

--- src/Frontend/src/Components/Reviews.jsx
+++ src/Frontend/src/Components/Reviews.jsx
@@ -20,6 +20,38 @@ export default function Reviews(props) {
     : null;
   const [starRating, setStarRating] = React.useState(0);
   const [mouseOverStar, setMouseOverStar] = React.useState(0);
+  const [review, setReview] = React.useState("");
+  function postReview() {
+    const apiUrl = "";
+    if (review.length === 0 || starRating === 0) {
+      return;
+    }
+    const postData = {
+      acmid: reviews.acmid,
+      rating: starRating,
+      review: review,
+    };
+    console.log(postData);
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+      })
+      .then(() => {
+        setStarRating(0);
+        setReview("");
+      })
+      .catch((error) => {
+        console.error("Error posting data:", error.message);
+      });
+  }
   return (
     <div className="reviews">
       <div className="review">
@@ -28,17 +60,25 @@ export default function Reviews(props) {
           id="myText"
           placeholder="Write your review!!"
           className="userrev"
+          value={review}
+          maxLength={500}
+          onChange={(event) => setReview(event.target.value)}
         ></textarea>
-        <div className="star-rating">
-          {[1, 2, 3, 4, 5].map((index) => (
-            <StarRating
-              key={index}
-              filled={index <= starRating || index <= mouseOverStar}
-              onClick={() => setStarRating(index)}
-              onMouseOver={() => setMouseOverStar(index)}
-              onMouseLeave={() => setMouseOverStar(0)}
-            />
-          ))}
+        <div className="rating-submit">
+          <div className="star-rating">
+            {[1, 2, 3, 4, 5].map((index) => (
+              <StarRating
+                key={index}
+                filled={index <= starRating || index <= mouseOverStar}
+                onClick={() => setStarRating(index)}
+                onMouseOver={() => setMouseOverStar(index)}
+                onMouseLeave={() => setMouseOverStar(0)}
+              />
+            ))}
+          </div>
+          <button className="button" onClick={postReview}>
+            Submit
+          </button>
         </div>
       </div>
       {reviewItems}
